335f49d0790b55d39d2a80d1ddc193ab748333ae
sapeled3 <oyinkuromosesvictor@gmail.com>
2019-08-06 00:12:37 +0100

    database declared

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is Working') })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, postgres, bcrypt)})
 
